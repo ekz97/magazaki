2521b0b729de7588c6b0e1c812962b904b2a048a
Luc Vervoort <luc.vervoort@hogent.be>
2023-06-02 15:44:37 +0200

    Magazaki

---
+++ PeasieCleanAll.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pkill -9 -f Peasie
+pkill -9 -f Magazaki
+pkill -9 -f Nestrix
+rm PeasieAPI.out
+rm MagazkiAPI.out
+rm NestrixAPI.out
---
+++ PeasieStopAll.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pkill -9 -f Peasie
+pkill -9 -f Magazaki
+pkill -9 -f Nestrix
